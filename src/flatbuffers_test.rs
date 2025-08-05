5e465e23e04f1698b41912e981a3ab08e65e253f
viktorneo2002 <viktorpetroski62@gmail.com>
2025-08-05 15:17:34 -0400

    Create flatbuffers_test.rs

---
+++ src/flatbuffers_test.rs
@@ -0,0 +1,33 @@
+use flatbuffers::{FlatBufferBuilder, WIPOffset, Vector, Table, ForwardsUOffset};
+
+// Use our generated FlatBuffers code
+use crate::flatbuffers_generated;
+
+use flatbuffers_generated::model_weights_generated::{ModelWeights, FloatVector, ModelWeightsArgs, FloatVectorArgs};
+
+fn main() {
+    let mut builder = FlatBufferBuilder::new();
+    
+    // Create a simple FloatVector
+    let values = vec![1.0, 2.0, 3.0];
+    let values_vec = builder.create_vector(&values);
+    let float_vector = FloatVector::create(&mut builder, &FloatVectorArgs {
+        values: Some(values_vec),
+    });
+    
+    // Create a simple ModelWeights
+    let model_weights = ModelWeights::create(&mut builder, &ModelWeightsArgs {
+        input_weights: None,
+        hidden_weights: None,
+        output_weights: None,
+        input_bias: None,
+        hidden_bias: None,
+        output_bias: None,
+        feature_count: 5,
+        hidden_size: 4,
+        output_size: 5,
+    });
+    
+    builder.finish(model_weights, None);
+    println!("FlatBuffers test completed successfully!");
+}
