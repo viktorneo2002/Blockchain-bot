e0b24bc5879fa62de4fd5cb0d493c28bff0525eb
viktorneo2002 <viktorpetroski62@gmail.com>
2025-09-03 14:06:34 -0400

    Update mod.rs

--- src/advanced_backtesting/mod.rs
+++ src/advanced_backtesting/mod.rs
@@ -1,3 +1,11 @@
+pub mod monte_carlo_shared;
 pub mod monte_carlo_simulator;
+pub mod offchain_worker;
+pub mod vrf_simulator;
+pub mod simd_mc;
 
-pub use monte_carlo_simulator::MonteCarloSimulator;
+pub use monte_carlo_shared::{SimulationConfig, SimulationResults, SimulationResultsFixed};
+pub use monte_carlo_simulator::run_monte_carlo;
+pub use offchain_worker::run_offchain_worker;
+pub use vrf_simulator::run_vrf_simulation;
+pub use simd_mc::run_simd_mc;
