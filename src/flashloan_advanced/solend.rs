bc883cc86563ca5058ae95d2bf5044b0d664326a
viktorneo2002 <viktorpetroski62@gmail.com>
2025-09-04 15:19:14 -0400

    Update solend.rs

--- src/flashloan_advanced/solend.rs
+++ src/flashloan_advanced/solend.rs
@@ -3,6 +3,16 @@ use solend_program::cpi;
 use solend_program::cpi::accounts::FlashBorrow as SolendFlashBorrow;
 use crate::state::ProtocolInfo;
 use crate::errors::ErrorCode;
+use async_trait::async_trait;
+use solana_sdk::{instruction::Instruction, pubkey::Pubkey};
+use solend_program::state::Obligation;
+
+use crate::{
+    adapters::adapter::{ProtocolAdapter, LiquidationOpportunity},
+    errors::OptimizerError,
+    optimizer::AccountHealth,
+    config::ProtocolConfig,
+};
 
 pub struct SolendAdapter<'a, 'info> {
     pub protocol_info: &'a mut Account<'info, ProtocolInfo>,
@@ -44,5 +54,48 @@ impl<'a, 'info> crate::adapters::FlashLoanAdapter<'info> for SolendAdapter<'a, '
 
     fn repay_flash_loan(&self, _amount: u64) -> Result<()> {
         Ok(())
+    }
+}
+
+pub struct SolendAdapterV2 {
+    program_id: Pubkey,
+    config: ProtocolConfig,
+}
+
+impl SolendAdapterV2 {
+    pub fn new(program_id: Pubkey, config: ProtocolConfig) -> Self {
+        Self { program_id, config }
+    }
+}
+
+#[async_trait]
+impl ProtocolAdapter for SolendAdapterV2 {
+    fn name(&self) -> &str {
+        "solend"
+    }
+
+    fn program_id(&self) -> Pubkey {
+        self.program_id
+    }
+
+    async fn scan_accounts(&self) -> Result<Vec<Pubkey>, OptimizerError> {
+        // Implementation would scan Solend obligations
+        Ok(vec![])
+    }
+
+    async fn parse_health(&self, acct_data: &[u8]) -> Option<AccountHealth> {
+        let obligation = Obligation::deserialize(acct_data).ok()?;
+        Some(AccountHealth {
+            health_factor: obligation.health(),
+            value_at_risk: obligation.unhealthy_borrow_value(),
+        })
+    }
+
+    async fn build_liquidation_ix(
+        &self,
+        opp: &LiquidationOpportunity,
+    ) -> Result<Vec<Instruction>, OptimizerError> {
+        // Build Solend liquidation instructions
+        Ok(vec![])
     }
 }
